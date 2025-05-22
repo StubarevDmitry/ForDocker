a8a74473b22f063a833b6f8f8f03d4b17acaf24c
stuba <d.stubarev@g.nsu.ru>
2025-05-22 18:45:36 +0700

    fix: проверка

--- ForDocker/Program.cs
+++ ForDocker/Program.cs
@@ -13,12 +13,3 @@ while (true)
     Console.WriteLine(a + b);
     Thread.Sleep(1000);
 }
-while (true)
-{
-    Console.WriteLine(a + b);
-    Thread.Sleep(1000);
-    Console.WriteLine(a + b);
-    Thread.Sleep(1000);
-    Console.WriteLine(a + b);
-    Thread.Sleep(1000);
-}
